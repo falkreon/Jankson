6404d6599518954237f7f4cfd065481120720c3c
Falkreon <falkreon@gmail.com>
2023-02-05 04:59:54 -0600

    Add boolean ValueParser

---
+++ src/main/java/blue/endless/jankson/impl/io/context/BooleanValueParser.java
@@ -0,0 +1,27 @@
+package blue.endless.jankson.impl.io.context;
+
+import java.io.IOException;
+
+import blue.endless.jankson.api.SyntaxError;
+import blue.endless.jankson.impl.io.Lookahead;
+import blue.endless.jankson.impl.io.LookaheadCodePointReader;
+
+public class BooleanValueParser implements ValueParser {
+
+	@Override
+	public boolean canRead(Lookahead lookahead) throws IOException {
+		String maybeFalse = lookahead.peekString(5);
+		return maybeFalse.equals("false") || maybeFalse.startsWith("true");
+	}
+
+	@Override
+	public Object read(LookaheadCodePointReader reader) throws IOException, SyntaxError {
+		String start = reader.readString(4);
+		if (start.equals("true")) return Boolean.TRUE;
+		start += reader.readString(1);
+		if (start.equals("false")) return Boolean.FALSE;
+		
+		throw new IllegalStateException("Couldn't parse boolean value '"+start+"'.");
+	}
+
+}
